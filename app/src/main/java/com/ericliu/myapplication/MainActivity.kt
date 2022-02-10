f63eddbdc995119b4906ead3ac4b6eb3aa560ecb
Eric Liu <eric.liu.developer@gmail.com>
2022-02-09 21:32:26 -0800

    initial commit

---
+++ app/src/main/java/com/ericliu/myapplication/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.ericliu.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ericliu.myapplication.ui.theme.AppLinkApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    private var greetingMessage = "Android"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppLinkApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting(greetingMessage)
+                }
+            }
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        handleIntent()
+    }
+
+    private fun handleIntent() {
+        intent.data?.let {
+            intent.data?.lastPathSegment?.let {
+                greetingMessage = it
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AppLinkApplicationTheme {
+        Greeting("Android")
+    }
+}
