1d206521a9bf5c3905e51c6d2db49585a9d03603
Eric Liu <eric.liu.developer@gmail.com>
2022-02-10 22:21:24 -0800

    more compose stuff

--- app/src/main/java/com/ericliu/myapplication/MainActivity.kt
+++ app/src/main/java/com/ericliu/myapplication/MainActivity.kt
@@ -4,18 +4,19 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -65,19 +66,30 @@ fun MyApp(message: MutableState<String>) {
         Column {
             MakeTitle(message = "Hello")
             MakeTitle(message = message.value)
+
+            Box(modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center) {
+                OutlinedButton(
+                    onClick = { /*TODO*/ },
+                    Modifier
+                        .padding(horizontal = 8.dp, vertical = 4.dp)
+                        .fillMaxWidth(0.8f)
+                ) {
+                    Text(text = "Click Me")
+                }
+            }
         }
-//        Text(
-//            text = "Hello ${message.value}!", fontSize = 25.sp, modifier = Modifier
-//                .padding(24.dp)
-//                .fillMaxWidth()
-//        )
     }
 }
 
 @Composable
 fun MakeTitle(message: String) {
     Text(
-        text = message, fontSize = 25.sp, modifier = Modifier
+        text = message,
+        textAlign = TextAlign.Center,
+        fontWeight = FontWeight.Bold,
+        fontSize = 25.sp,
+        modifier = Modifier
             .padding(24.dp)
             .fillMaxWidth()
     )
