57cc08c3392f4d7066673fcd8730eb8e72217be8
Eric Liu <eric.liu.developer@gmail.com>
2022-02-10 22:08:12 -0800

    adds more ui

--- app/src/main/java/com/ericliu/myapplication/MainActivity.kt
+++ app/src/main/java/com/ericliu/myapplication/MainActivity.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -14,8 +17,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ericliu.myapplication.ui.theme.AppLinkApplicationTheme
 
+/**
+ * Launch from https://ericliu001.github.io/user/eric
+ */
 class MainActivity : ComponentActivity() {
     private var greetingMessage = mutableStateOf("Android")
 
@@ -28,7 +36,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting(greetingMessage)
+                    MyApp(greetingMessage)
                 }
             }
         }
@@ -52,15 +60,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(message: MutableState<String>) {
-    Text(text = "Hello ${message.value}!")
+fun MyApp(message: MutableState<String>) {
+    Surface(color = MaterialTheme.colors.primary) {
+        Column {
+            MakeTitle(message = "Hello")
+            MakeTitle(message = message.value)
+        }
+//        Text(
+//            text = "Hello ${message.value}!", fontSize = 25.sp, modifier = Modifier
+//                .padding(24.dp)
+//                .fillMaxWidth()
+//        )
+    }
+}
+
+@Composable
+fun MakeTitle(message: String) {
+    Text(
+        text = message, fontSize = 25.sp, modifier = Modifier
+            .padding(24.dp)
+            .fillMaxWidth()
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     AppLinkApplicationTheme {
-        Greeting(remember {
+        MyApp(remember {
             mutableStateOf("Android")
         })
     }
